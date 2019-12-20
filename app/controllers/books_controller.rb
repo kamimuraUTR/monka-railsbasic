e657d9f5bb30b6504189eb2ffab9b03c0aa96606
kamimuraUTR <guitarvo.motti@gmail.com>
2019-12-20 11:25:48 +0900

    book image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -64,6 +64,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :published_on, :price, :displayed)
+      params.require(:book).permit(:title, :author, :published_on, :price, :displayed, :image)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :phone_number)
+      params.require(:user).permit(:name, :phone_number, :photo)
+      #paramsはuserのnameとphone_numberとphotoの値があることだけを許可している
     end
 end
